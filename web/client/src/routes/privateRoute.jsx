a9e20f2d09627a9754e5fd1ce01a7ff844aa680d
swadhinroutray <swadhin.routray@gmail.com>
2021-04-26 21:57:09 +0530

    Favicon and SEQ

--- web/client/src/routes/privateRoute.jsx
+++ web/client/src/routes/privateRoute.jsx
@@ -5,6 +5,18 @@ import loginStore from '../models/loginModel';
 
 export const PrivateRoute = inject('loginStore')(
 	observer(({ component: Component, ...rest }) => {
+		// return (
+		// 	<Route
+		// 		render={(props, loginStore) =>
+		// 			!loginStore.loggedIn ? (
+		// 				<Redirect to="/login" />
+		// 			) : (
+		// 				<Component {...props} />
+		// 			)
+		// 		}
+		// 	></Route>
+		// );
+
 		return !loginStore.loggedIn ? (
 			<Redirect to="/login" />
 		) : (
